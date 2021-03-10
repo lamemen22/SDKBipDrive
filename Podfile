61b8335a8b7b43397a0fb4124148442a8440eacc
isegura <isegura@deloitte.es>
2021-03-10 08:50:33 +0100

    Initial commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+
+platform :ios, '9.0'
+
+target 'SDKBipDrive' do
+    pod 'Alamofire', '4.9.1'
+    pod 'MBProgressHUD', '~> 1.1.0', :modular_headers => true
+end
---
+++ SDKBipDrive/SDKBipDrive.h
@@ -0,0 +1,18 @@
+//
+//  SDKBipDrive.h
+//  SDKBipDrive
+//
+//  Created by admin on 08/03/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDKBipDrive.
+FOUNDATION_EXPORT double SDKBipDriveVersionNumber;
+
+//! Project version string for SDKBipDrive.
+FOUNDATION_EXPORT const unsigned char SDKBipDriveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKBipDrive/PublicHeader.h>
+
+
